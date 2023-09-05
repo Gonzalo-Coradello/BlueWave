0f0ec9a2d7b626cf5838f2aa7f1e467646d1c37d
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-09-05 19:31:20 -0300

    add more services and refactor

---
+++ src/components/ServiceCard.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import servicexd from '../assets/images/service-icon-2.png'
+
+const ServiceCard = ({ service, index }) => {
+  const [expandedCardIndex, setExpandedCardIndex] = useState(null)
+
+  const handleMouseEnter = index => {
+    setExpandedCardIndex(index)
+  }
+
+  const handleMouseLeave = () => {
+    setExpandedCardIndex(null)
+  }
+
+  return (
+    <li
+      onMouseEnter={() => handleMouseEnter(index)}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div
+        className={`service-card ${
+          expandedCardIndex === index ? 'expanded' : ''
+        }`}
+      >
+        <div className='card-icon'>
+          <img
+            src={servicexd}
+            width='30'
+            height='30'
+            loading='lazy'
+            alt='icon'
+          />
+        </div>
+        <h3 className='h4 text-[2.1rem] leading-[2.8rem] font-medium'>
+          <a href='#' className='card-title'>
+            {service.title}
+          </a>
+        </h3>
+        <p className='card-description mt-6 pt-4 border-t'>{service.content}</p>
+      </div>
+    </li>
+  )
+}
+
+export default ServiceCard
---
+++ src/pages/Services/servicesData.js
@@ -0,0 +1,60 @@
+export const servicesData = [
+  {
+    title: 'Diseño web personalizado y responsive.',
+    content:
+      'Creamos sitios web personalizados y adaptables a todos los dispositivos para brindar la mejor experiencia al usuario.',
+  },
+  {
+    title: 'Desarrollo de sitios web y aplicaciones.',
+    content:
+      'Desarrollamos sitios web y aplicaciones web a medida, utilizando tecnologías modernas y las mejores prácticas de desarrollo.',
+  },
+  {
+    title: 'Optimización para buscadores (SEO).',
+    content:
+      'Mejoramos el posicionamiento de tu sitio web en los motores de búsqueda para que puedas llegar a más clientes potenciales.',
+  },
+  {
+    title: 'Diseño de logotipos y branding digital.',
+    content:
+      'Creamos logotipos y elementos de branding digitales que representen la identidad de tu marca y conecten con tu audiencia.',
+  },
+  {
+    title: 'Gestión y mantenimiento de sitios web.',
+    content:
+      'Nos encargamos del mantenimiento y actualización de tu sitio web para que puedas centrarte en hacer crecer tu negocio.',
+  },
+]
+
+export const extraServices = [
+  {
+    title: 'Análisis de Páginas Web',
+    content:
+      '¿Quieres saber cómo mejorar tu presencia en línea? Nuestro análisis de páginas web te proporciona una evaluación detallada de tu sitio, identificando áreas de mejora para optimizar el rendimiento y la usabilidad. Descubre cómo puedes destacarte en la web.',
+  },
+  {
+    title: 'Asesoría en programación y Diseño de Páginas Web',
+    content:
+      'Nuestra experiencia en programación y diseño web está a tu disposición. Te brindamos asesoría personalizada para crear un sitio web atractivo y funcional que capte la atención de tu audiencia. Desde la idea hasta la implementación, estamos aquí para ayudarte a materializar tu visión en línea.',
+  },
+  {
+    title: 'Servicios de Marketing',
+    content:
+      'Impulsa tu negocio al siguiente nivel con nuestros servicios de marketing. Desde estrategias de redes sociales hasta publicidad en línea, te ayudamos a llegar a tu público objetivo y a construir una presencia sólida en línea. Aumenta la visibilidad de tu marca con nuestras soluciones de marketing probadas.',
+  },
+  {
+    title: 'Armado y Asesoría de LinkedIn',
+    content:
+      'Tu perfil de LinkedIn es tu carta de presentación profesional en línea. Ofrecemos servicios de armado y asesoría personalizada para que tu perfil destaque y te ayude a conectarte con oportunidades laborales y profesionales. Maximiza tu presencia en la red de negocios más grande del mundo.',
+  },
+  {
+    title: 'Armado y Asesoría de CV (Currículum Vitae)',
+    content:
+      'Tu CV es crucial para tu carrera. Nuestro servicio de armado y asesoría de CV te ayuda a destacar tus habilidades y experiencia de manera efectiva. Asegúrate de causar una impresión positiva en los empleadores y aumenta tus posibilidades de conseguir el trabajo que deseas.',
+  },
+  {
+    title: 'Armado y Asesoría de Portafolio',
+    content:
+      'Tu portafolio es la vitrina de tus logros y habilidades. Ofrecemos servicios de armado y asesoría de portafolio para que puedas presentar tus proyectos y trabajos de manera impactante. Destaca lo que te hace único y atrae oportunidades profesionales.',
+  },
+]
